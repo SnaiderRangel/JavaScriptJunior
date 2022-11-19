b5620c843fb5281c9d5fea1ba7381a4e1413607c
Snaider Rangel <xtrack1921@gmail.com>
2022-11-18 23:33:22 -0500

    Practica de logica 1

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
-const colores = ['Azul', 'Verde', 'Amarrillo', 'Negro'];
+const usuarios = [];
+const contraseñas = [];
+
+for(i = 0; i < 2; i++){
+    nameOfUser = prompt('Nombre de usuario');
+    paswordOfUser = prompt('Contraseña');
+
+    usuarios[i] = nameOfUser;
+    contraseñas[i] = paswordOfUser;
+    document.write(`La contraseña de ${usuarios[i]} es ${contraseñas[i]}<br>`);
+    if (i == 1){
+        document.write('<br>' + usuarios);
+    }
+}
