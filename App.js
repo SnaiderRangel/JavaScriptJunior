a533ac1d894b305922cbfa6fb7a1197eed59b92f
Snaider Rangel <xtrack1921@gmail.com>
2022-11-20 11:28:02 -0500

    Sistema de entrada vip

--- App.js
+++ App.js
@@ -54,34 +54,59 @@
 
 // }
 
-const NumeroDeEstudiantes = parseInt(prompt('number of students: '));
-const diasDelMes = prompt('number of days in the month'); 
-let estudiantes = [];
-let x = 0;
-let a = 0; 
-let f = 0;
+// const NumeroDeEstudiantes = parseInt(prompt('number of students: '));
+// const diasDelMes = prompt('number of days in the month'); 
+// let estudiantes = [];
+// let x = 0;
+// let a = 0; 
+// let f = 0;
 
-let validarAsistencias = () =>{
-    if (a < f){
-       return document.write(`${estudiantes[i]}: Reprobo ${'<br>'}`);
-    }
-    else if(a > f){
-       return document.write(`${estudiantes[i]}: Aprobo ${'<br>'}`);
-    }
-}
+// let validarAsistencias = () =>{
+//     if (a < f){
+//        return document.write(`${estudiantes[i]} tiene ${f} Faltas y ${a} Asistencias de los ${diasDelMes} dias del mes: <b style="background-color:red ;">Reprobo</b> ${'<br>'}`);
+//     }
+//     else if(a > f){
+//        return document.write(`${estudiantes[i]} tiene ${f} Faltas y ${a} Asistencias de los ${diasDelMes} dias del mes: <b>Aprobo</b> ${'<br>'}`);
+//     }
+// }
 
-for(i = 1; i<= NumeroDeEstudiantes; i++){
-    estudiantes[i] = prompt(`name of student ${i}`);
-    while(x <= diasDelMes){
-        asistencias = prompt('A si asistio f si falto') 
-        if (asistencias == 'a' || asistencias == 'A'){
-            a++;
-        } 
-        else if(asistencias == 'f' || asistencias == 'F'){
-            f++;
+// for(i = 1; i<= NumeroDeEstudiantes; i++){
+//     estudiantes[i] = prompt(`name of student ${i}`);
+//     while(x <= diasDelMes){
+//         asistencias = prompt('A si asistio f si falto') 
+//         if (asistencias == 'a' || asistencias == 'A'){
+//             a++;
+//         } 
+//         else if(asistencias == 'f' || asistencias == 'F'){
+//             f++;
+//         }
+//         x++;
+//     }
+//     x = 0;
+//     validarAsistencias(); 
+// }
+let edad;
+let time;
+let vip = true;
+
+let entradaVip = () =>{
+    edad = prompt('Edad: ');
+    if (edad >= 18){
+        time = prompt('Current time: ');
+        if (time >= '2am' && vip){
+            vip = false;
+            return alert(`Puedes pasar gratis por que son las: ${time}`);
         }
-        x++;
+        else{
+            return alert(`El valor de la entrada es de $5 Dolares: `);
+        }
     }
-    x = 0;
-    validarAsistencias(); 
+    else(
+        alert('No se permiten menores de 18 años')
+    )
 }
+
+entradaVip();
+entradaVip();
+entradaVip();
+entradaVip();
