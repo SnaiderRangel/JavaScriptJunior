b1dad4333f71f03d07759271f123e2049f8cd3ff
Snaider Rangel <xtrack1921@gmail.com>
2022-11-19 21:36:18 -0500

    Creación interfaz grafica

--- App.js
+++ App.js
@@ -26,12 +26,50 @@
 // document.write(`${nombreDeEquipo} <br> ${nombreDelProcesador}`);
 
 
+// const NumeroDeEstudiantes = parseInt(prompt('number of students: '));
+// const diasDelMes = prompt('number of days in the month'); 
+// let estudiantes = [];
+// let x = 0;
+// let a = 0; 
+// let f = 0;
+// for(i = 1; i<= NumeroDeEstudiantes; i++){
+//     estudiantes[i] = prompt(`name of student ${i}`);
+//     while(x <= diasDelMes){
+//         asistencias = prompt('A si asistio f si falto') 
+//         if (asistencias == 'a' || asistencias == 'A'){
+//             a++;
+//         } 
+//         else if(asistencias == 'f' || asistencias == 'F'){
+//             f++;
+//         }
+//         x++;
+//     }
+//     x = 0; 
+//     if (a < f){
+//         document.write(`${estudiantes[i]}: Reprobo ${'<br>'}`)
+//     }
+//     else if(a > f){
+//         document.write(`${estudiantes[i]}: Aprobo ${'<br>'}`);
+//     }
+
+// }
+
 const NumeroDeEstudiantes = parseInt(prompt('number of students: '));
 const diasDelMes = prompt('number of days in the month'); 
 let estudiantes = [];
 let x = 0;
 let a = 0; 
 let f = 0;
+
+let validarAsistencias = () =>{
+    if (a < f){
+       return document.write(`${estudiantes[i]}: Reprobo ${'<br>'}`);
+    }
+    else if(a > f){
+       return document.write(`${estudiantes[i]}: Aprobo ${'<br>'}`);
+    }
+}
+
 for(i = 1; i<= NumeroDeEstudiantes; i++){
     estudiantes[i] = prompt(`name of student ${i}`);
     while(x <= diasDelMes){
@@ -44,12 +82,6 @@ for(i = 1; i<= NumeroDeEstudiantes; i++){
         }
         x++;
     }
-    x = 0; 
-    if (a < f){
-        document.write(`${estudiantes[i]}: Reprobo ${'<br>'}`)
-    }
-    else if(a > f){
-        document.write(`${estudiantes[i]}: Aprobo ${'<br>'}`);
-    }
-
+    x = 0;
+    validarAsistencias(); 
 }
